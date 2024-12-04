1e4dde1d38ceed00a7653a2d0b3232e3ebe3d6fd
kchabin <kchabin@swu.ac.kr>
2024-12-04 16:29:48 +0900

    fixed untracked files

--- springboot/src/main/kotlin/swu/pbl/ppap/auth/common/authority/TokenInfo.kt
+++
@@ -1,7 +0,0 @@
-package swu.pbl.ppap.auth.common.authority
-
-data class TokenInfo(
-    val grantType: String, //jwt 권한 인증 타입
-    val accessToken: String, //실제 검증 시 확인할 토큰
-    val refreshToken: String,
-)
--- springboot/src/main/kotlin/swu/pbl/ppap/auth/user/repository/UserRoleRepository.kt
+++
@@ -1,8 +0,0 @@
-package swu.pbl.ppap.auth.user.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import swu.pbl.ppap.auth.user.entity.UserRole
-
-
-interface UserRoleRepository : JpaRepository<UserRole, Long> {
-}
